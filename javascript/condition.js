0b968cdd7c1ab1d1326ceb29818d423c5916c238
PrashantS09 <i.prashantsingh007@gmail.com>
2025-03-11 11:38:40 +0530

    javascript

---
+++ javascript/condition.js
@@ -0,0 +1,11 @@
+num = 18
+
+if (num % 2 ===0) {
+
+   const a = 'nice';
+    console.log( 'even');
+} else {
+    console.log( 'odd');
+}
+
+console.log(a);
---
+++ javascript/index.js
@@ -0,0 +1,19 @@
+console.log('javascript is awesome');
+
+num= 56;
+
+console.log(num);
+
+console.log(typeof num);
+
+myname = 'lestrade';
+
+console.log(myname);
+
+console.log( 1 ==1 );
+console.log( 1 == '1');
+console.log( 1 === '1');
+
+console.log( 1+1 );
+console.log( 1+'1' );
+console.log( '1'+'1'+ '1');
